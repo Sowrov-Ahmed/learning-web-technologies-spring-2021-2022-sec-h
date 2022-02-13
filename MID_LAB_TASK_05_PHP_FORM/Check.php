21794fd00c551c1c1c5b19b782627f17a4bc2cc9
Sowrov Ahmed <s.ahmed125455@gmail.com>
2022-02-13 15:00:43 +0600

    Lab Files

---
+++ MID_LAB_TASK_05_PHP_FORM/Check.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_REQUEST['submit']))
+{
+    $name = $_REQUEST['name'];
+    if($name == ""){
+        echo "null value...";
+    }
+    
+    else{
+        echo $name;
+    }	
+}
+
+else{
+    echo "Invalid request...";
+}
+
+?>
---
+++ MID_LAB_TASK_05_PHP_FORM/Task04.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+
+    <title>page 3</title>
+
+</head>
+
+<body>
+    <form method="post" action="check.php">
+       <fieldset>
+            <table border=".5">
+                <tr>
+                    <td>Gender: </td>
+                    <td>
+                        <input type="radio" name="gender" value=""> Male
+                        <input type="radio" name="gender" value=""> Female
+                        <input type="radio" name="gender" value=""> Other
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+
+                        <input type="submit" name="submit" value="Submit">
+
+                    </td>
+                </tr>
+
+
+            </table>
+        </fieldset>
+    </form>
+
+</body>
+
+</html>
